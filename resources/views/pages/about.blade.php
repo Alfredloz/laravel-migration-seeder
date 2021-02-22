60e157b5bac6b100b480c702079affe3de7d4d50
Alfredloz <alfredogregorio.lozano@gmail.com>
2021-02-22 14:55:47 +0100

    creazione dei file blade e impostazione del layout

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title')
+    About
+@endsection
+@section('content')
+    <h1>About section</h1>
+@endsection
---
+++ resources/views/pages/contacts.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title')
+    About
+@endsection
+@section('content')
+    <h1>Contacts section</h1>
+@endsection
